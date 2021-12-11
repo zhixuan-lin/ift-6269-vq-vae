acd4730f0cbaa729d3046b8dc7e4967764da2ac2
Xutong Zhao <16513333+TongTongX@users.noreply.github.com>
2021-12-10 19:46:41 -0500

    scripts for tiny imagenet

---
+++ scripts/run_vanilla_dl_tiny_imagenet.py
@@ -0,0 +1,31 @@
+import torch
+import numpy as np
+import argparse
+import os.path as osp
+from vqvae_lib.api import train_vanilla_vae
+from vqvae_lib.data import TinyImagenet
+from vqvae_lib.utils import SingleTensorDataset
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='./data', help='data_dir/tiny_imagenet200 will contain the dataset')
+    parser.add_argument('--result_dir', type=str, default='./results/vanilla', help='where to save everything (model/tensorboard/images...)')
+    args = parser.parse_args()
+
+    train_dataset = TinyImagenet(args.data_dir, TinyImagenet.TRAIN)
+    val_dataset = TinyImagenet(args.data_dir, TinyImagenet.VALIDATION)
+
+    # Adapt to other dataset: change image_size, train_dataset and val_dataset
+    train_vanilla_vae(
+        image_size=64,
+        train_dataset=train_dataset,
+        val_dataset=val_dataset,
+        result_dir=args.result_dir,
+        exp_name='vanilla_vae_dl_tiny_imagenet',
+        loss_type='discretized_logistic',
+        epochs=100,
+    )
+    # train_data, val_data = np.zeros((100, 32, 32, 3)), np.zeros((10000, 32, 32, 3))
+
+if __name__ == '__main__':
+    main()
